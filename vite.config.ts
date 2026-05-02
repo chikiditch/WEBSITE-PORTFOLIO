2843ce93d2a002861d7f1fe8efce5afa9c8984fc
chikiditch <cabeza.edgarjr@ue.edu.ph>
2026-05-03 04:25:30 +0800

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
-  }
-})
+  },
+});
